aed60afc3c8f6392955efb5e83c3316093a08a8b
Mariya Shchepina <marishchepina@gmail.com>
2021-06-09 09:24:18 +0300

    Dialogs component refactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Dialogs.css'
 
 
+const  Interlocutor= (props) => {
+    let path =`/dialogs/${props.id}`
+    return(
+        <div className="interlocutor">
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+};
+
+const  Message = (props) => {
+    let path =`/dialogs/${props.id}`
+    return(
+        <div className="message">
+            {props.message}
+        </div>
+    )
+};
+
 const Dialogs = () => {
     return( 
-    <div>
-        dialogs
-    </div> 
+    <div className="dialogs-wrap">
+        <div className="interlocutor-wrap">
+            <Interlocutor name="Dima" id="1" />
+            <Interlocutor name="Lera" id="2" />
+            <Interlocutor name="Anya" id="3" />
+            <Interlocutor name="Max" id="4" />
+            <Interlocutor name="Yarik" id="5" />
+        </div> 
+        <div className="messages-wrap">
+            <Message message="Hi"/>
+            <Message message="Hello"/>
+            <Message message="How are you?"/>
+        </div>
+    </div>
     );
 }
 
