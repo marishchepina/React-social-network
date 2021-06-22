910bc1f049e9cf2b25d19f2fb38a5f4cfbf709ec
Mariya Shchepina <marishchepina@gmail.com>
2021-06-22 19:12:30 +0300

    Preloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Preloader.css'
+
+const Preloader = () => {
+  return (
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Preloader
