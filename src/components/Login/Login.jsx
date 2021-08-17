c0e4b86989015be461b05c14ed497ee12cb1bfcb
Mari Shchepina <marishchepina@gmail.com>
2021-08-17 20:31:27 +0300

    redux-form for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const Login = () => {
-  return <h1>Login</h1>
+const LoginForm = (props) => {
+  return (
+    <div>
+      <form onSubmit={props.handleSubmit}>
+        <Field placeholder={'Login'} component={'input'} name={'login'} />
+        <br />
+        <br />
+        <Field placeholder={'Password'} component={'input'} name={'password'} />
+        <br />
+        <br />
+        <Field type={'checkbox'} component={'input'} name={'rememberMe'} />
+        <label>Remember me</label>
+        <br />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm) //'login' unic name for my form
+//store.getState().form
+
+const Login = (props) => {
+  const onSubmit = (formData) => {
+    console.log(formData)
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmitg} />
+    </div>
+  )
 }
 
 export default Login
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -6,6 +6,7 @@ import usersReducer from './users-reducer'
 import authReducer from './auth-reducer'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { reducer as formReducer } from 'redux-form'
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 let reducers = combineReducers({
@@ -14,6 +15,7 @@ let reducers = combineReducers({
   sidebar: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 })
 
 let store = createStore(
